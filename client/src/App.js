30664a7adcdf5bf8635cbb186e36271961f996d9
shivaverma1115 <shivaverma1115@gmail.com>
2023-11-06 12:50:40 +0530

    update

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./Components/Navbar";
 import AllRouter from "./AllRouter/AllRouter";
 
 function App() {
+  console.log(process.env.REACT_APP_URL_LINK) ;
   return (
     <Box>
       <Navbar/>
--- client/src/Components/RegisterUser.jsx
+++ client/src/Components/RegisterUser.jsx
@@ -1,6 +1,6 @@
 import { Avatar, Box, Button, FormControl, FormLabel, MenuItem, Radio, RadioGroup, Select } from '@mui/material'
 import { deepPurple } from '@mui/material/colors'
-import React from 'react'
+import React, { useState } from 'react'
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
@@ -8,15 +8,30 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 import { useNavigate } from 'react-router-dom';
 
 const RegisterUser = () => {
-  const nevigate = useNavigate() ;
-  const handleGoToHome = ()=>{
+  const nevigate = useNavigate();
+  const handleGoToHome = () => {
     nevigate('/')
+  }
+  const [data, setData] = useState([]);
+  const handleInput = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value })
+  }
+  const handleSubmit = async () => {
+     const res = await fetch(`http://localhost:8080/register`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+      })
+      const ans = await res.json() ;
+      console.log(ans) ;
   }
   return (
     <Box>
       <Button onClick={handleGoToHome} style={{ margin: '20px' }} variant="contained">
         Back to Home
-        </Button>
+      </Button>
       <Box width={'fit-content'} m={'auto'} >
         <Typography variant="h6" gutterBottom>
           Register Your Details
@@ -28,46 +43,54 @@ const RegisterUser = () => {
       <Box width={'70%'} m={'auto'} >
         <Grid container spacing={3}>
           <Grid item xs={12} sm={6}>
-            <TextField type='text' required id="firstName" name="firstName" label="First name" fullWidth autoComplete="given-name" variant="standard" />
+            <TextField type='text' required id="firstName" name="first_name" label="First name" fullWidth autoComplete="given-name" variant="standard" onChange={(e) => handleInput(e)} />
           </Grid>
           <Grid item xs={12} sm={6}>
-            <TextField type='text' required id="LastName" name="LastName" label="Last name" fullWidth autoComplete="given-name" variant="standard" />
+            <TextField type='text' required id="LastName" name="last_name" label="Last name" fullWidth autoComplete="given-name" variant="standard" onChange={(e) => handleInput(e)} />
           </Grid>
           <Grid item xs={12} sm={6}>
-            <TextField type='email' required id="Email address" name="Email address" label="Email address" fullWidth autoComplete="given-name" variant="standard" />
+            <TextField type='email' required id="Email address" name="email" label="Email address" fullWidth autoComplete="given-name" variant="standard" onChange={(e) => handleInput(e)} />
           </Grid>
           <Grid item xs={12} sm={6}>
-            <TextField type='number' required id="Mobile" name="Mobile" label="Mobile" fullWidth autoComplete="given-name" variant="standard" />
+            <TextField type='number' required id="Mobile" name="mobile_no" label="mobile_no" fullWidth autoComplete="given-name" variant="standard" onChange={(e) => handleInput(e)} />
           </Grid>
           <Grid item xs={12} sm={6}>
-            <TextField type='text' required id="Enter Your Location" name="Enter Your Location" label="Enter Your Location" fullWidth autoComplete="given-name" variant="standard" />
+            <TextField type='text' required id="Enter Your Location" name="location" label="Enter Your Location" fullWidth autoComplete="given-name" variant="standard" onChange={(e) => handleInput(e)} />
           </Grid>
           <Grid item xs={12} sm={6}>
             <label>Select Your Profile</label>
-            <TextField type='file' required id="Select Your Profile" name="Select Your Profile" fullWidth autoComplete="given-name" variant="standard" />
+            <TextField type='file' required id="Select Your Profile" name="profile_img" fullWidth autoComplete="given-name" variant="standard" onChange={(e) => handleInput(e)} />
           </Grid>
           <Grid item xs={12} sm={6}>
-            <FormControl>
+            <FormControl onChange={(e) => handleInput(e)} >
               <FormLabel id="demo-row-radio-buttons-group-label">Gender</FormLabel>
               <RadioGroup
                 row
                 aria-labelledby="demo-row-radio-buttons-group-label"
                 name="row-radio-buttons-group"
               >
-                <FormControlLabel value="male" control={<Radio />} label="Male" />
-                <FormControlLabel value="female" control={<Radio />} label="Female" />
+                <FormControlLabel value="male" name='gender' control={<Radio />} label="Male" />
+                <FormControlLabel value="female" name='gender' control={<Radio />} label="Female" />
               </RadioGroup>
             </FormControl>
           </Grid>
           <Grid item xs={12} sm={6}>
-            <label>Select Your Status</label>
-            <Select required id="Select Your Status" name="Select Your Status" fullWidth autoComplete="given-name">
-              <MenuItem>Active</MenuItem>
-              <MenuItem>InActive</MenuItem>
-            </Select>
+            <FormControl onChange={(e) => handleInput(e)} >
+              <FormLabel id="demo-row-radio-buttons-group-label">Status</FormLabel>
+              <RadioGroup
+                row
+                aria-labelledby="demo-row-radio-buttons-group-label"
+                name="row-radio-buttons-group"
+              >
+                <FormControlLabel value="active" name='status' control={<Radio />} label="Active" />
+                <FormControlLabel value="inactive" name='status' control={<Radio />} label="Inactive" />
+              </RadioGroup>
+            </FormControl>
           </Grid>
         </Grid>
-        <Button style={{ marginTop: '20px' }} fullWidth variant="contained">Contained</Button>
+        <Button style={{ marginTop: '20px' }} fullWidth variant="contained" onClick={handleSubmit} >
+          Contained
+        </Button>
       </Box>
     </Box>
   )
